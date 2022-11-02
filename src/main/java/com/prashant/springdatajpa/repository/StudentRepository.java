71c84a66c56643462672c2ee58a6a679231aca67
prashantkr658 <prashantkr658@gmail.com>
2022-11-02 13:13:49 +0530

    Student with Guardian

--- src/main/java/com/prashant/springdatajpa/repository/StudentRepository.java
+++ src/main/java/com/prashant/springdatajpa/repository/StudentRepository.java
@@ -10,4 +10,7 @@ import java.util.List;
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
     List<Student> findByFirstName(String firstName);
+
+    List<Student> findByFirstNameContaining(String str);
+
 }
--- src/test/java/com/prashant/springdatajpa/repository/StudentRepositoryTest.java
+++ src/test/java/com/prashant/springdatajpa/repository/StudentRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.prashant.springdatajpa.repository;
 
+import com.prashant.springdatajpa.entity.Guardian;
 import com.prashant.springdatajpa.entity.Student;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,9 +20,9 @@ class StudentRepositoryTest {
                 .emailId("pkprashant@gmail.com")
                 .firstName("Prashant")
                 .lastName("Kumar")
-                .guardianName("Rahul")
+                /*.guardianName("Rahul")
                 .guardianEmail("rahul@gmail.com")
-                .guardianMobile("9878999965")
+                .guardianMobile("9878999965")*/
                 .build();
         studentRepository.save(student);
     }
@@ -36,5 +37,29 @@ class StudentRepositoryTest {
     public void getStudentByFirstName(){
         List<Student> students = studentRepository.findByFirstName("Prashant");
         System.out.println(students);
+    }
+
+    @Test
+    public void getStudentByFirstNameContaining(){
+        List<Student> students = studentRepository.findByFirstNameContaining("sh");
+        System.out.println(students);
+    }
+
+    @Test
+    public void saveStudentWithGuardian(){
+        Guardian guardian = Guardian.builder()
+                .name("Santosh")
+                .email("santosh@gmail.com")
+                .mobile("97898798999")
+                .build();
+
+        Student student = Student.builder()
+                .firstName("Mohit")
+                .lastName("Kukreti")
+                .emailId("mohit@gmail.com")
+                .guardian(guardian)
+                .build();
+
+        studentRepository.save(student);
     }
 }
